ea36d44453e37234dd42c3e80d31418644d74598
John <32995023+MrJohndoesntdrink@users.noreply.github.com>
2017-11-16 05:37:51 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+$(function(){
+	$("#btn1").click(function(){
+		$(".main-text>p").css("color","blue");
+	});  
+	$("#btn2").click(function(){
+		$(".main-text>p").css("color","yellow")
+	});  
+	$("#btn3").click(function(){
+		$(".main-text>p").css("color","red");
+	}); 
+	$("#btn4").click(function(){
+		$(".main-text>p").css("font-size","16px")
+	});  
+	$("#btn5").click(function(){
+		$(".main-text>p").css("font-size","20px");
+	}); 
+	$('type[button]')
+
+
+
+
+
+})
